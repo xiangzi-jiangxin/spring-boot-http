577f161803193f0dc7b85274a4c972ba88388da5
xiangzi <xiangzi.jiangxin@gmail.com>
2017-02-27 19:51:38 +0800

    undertow support with http2

--- src/main/java/cc/jiangxin/test/SpringBootHttpApplication.java
+++ src/main/java/cc/jiangxin/test/SpringBootHttpApplication.java
@@ -1,14 +1,30 @@
 package cc.jiangxin.test;
 
+import io.undertow.UndertowOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Configuration
 @RestController
 public class SpringBootHttpApplication {
+
+    @Bean
+    public EmbeddedServletContainerCustomizer tomcatCustomizer() {
+        return (container) -> {
+            if (container instanceof UndertowEmbeddedServletContainerFactory) {
+                ((UndertowEmbeddedServletContainerFactory) container)
+                        .addBuilderCustomizers((connector) -> connector.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
+            }
+        };
+    }
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String test() {
