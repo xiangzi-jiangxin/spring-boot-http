0804671b522a2d0ffeaf43d54aad26fc1811f610
xiangzi <xiangzi.jiangxin@gmail.com>
2017-02-27 17:37:43 +0800

    tomcat support with http2

--- src/main/java/cc/jiangxin/test/SpringBootHttpApplication.java
+++ src/main/java/cc/jiangxin/test/SpringBootHttpApplication.java
@@ -1,7 +1,11 @@
 package cc.jiangxin.test;
 
+import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpringBootHttpApplication {
+
+    @Bean
+    public EmbeddedServletContainerCustomizer tomcatCustomizer() {
+        return (container) -> {
+            if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                ((TomcatEmbeddedServletContainerFactory) container)
+                        .addConnectorCustomizers((connector) -> connector.addUpgradeProtocol(new Http2Protocol()));
+            }
+        };
+    }
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String test() {
